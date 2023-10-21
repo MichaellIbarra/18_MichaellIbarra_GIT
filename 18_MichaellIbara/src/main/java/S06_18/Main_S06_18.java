d677f25d6600d1302756746225b437808a4ca100
Michaell Ibarra <michaell.ibarra@vallegrande.edu.pe>
2023-10-21 17:43:46 -0500

    presentado

---
+++ 18_MichaellIbara/src/main/java/S06_18/Main_S06_18.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package S06_18;
+
+/**
+ *
+ * @author Matichelo
+ */
+public class Main_S06_18 {
+
+    public static void main(String[] args) {
+        System.out.println("Hola Mundo");
+    }
+}
